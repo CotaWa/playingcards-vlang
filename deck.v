a463cf93ab51a46b099db850c708aa30fc859cde
CotaWa <collin@snogerup.com>
2025-09-27 20:30:33 +0200

    Added the first function of the module part deck.

--- deck.v
+++ deck.v
@@ -1,3 +1,18 @@
 module playingcards
 
 import rand
+
+pub struct Deck {
+	pub mut:
+		cards []Card
+		drawn int = 0
+		remaining int = 52
+}
+
+pub fn generate_deck() []Card {
+	mut cards := []Card{}
+	for suit in 0 .. 4
+		for value in 1 .. 14 {
+			cards << card.card(vaule, suit)
+		}
+}
